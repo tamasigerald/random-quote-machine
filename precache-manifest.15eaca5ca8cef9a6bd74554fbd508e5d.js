6983a17663d9956689a7ec335c2f154047fde0eb
Gerald Andrei Tamasi <gerald@geraldlinux.home>
2019-07-28 21:43:20 +0200

    Updates

---
+++ precache-manifest.15eaca5ca8cef9a6bd74554fbd508e5d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45f756eff34ed8b9853c8f17625ac3df",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "c6be3def1924b2e3f744",
+    "url": "/random-quote-machine/static/css/2.7dce770a.chunk.css"
+  },
+  {
+    "revision": "cc7aa0ab80c0e4e1cb37",
+    "url": "/random-quote-machine/static/css/main.388ed2da.chunk.css"
+  },
+  {
+    "revision": "c6be3def1924b2e3f744",
+    "url": "/random-quote-machine/static/js/2.56fa35b2.chunk.js"
+  },
+  {
+    "revision": "cc7aa0ab80c0e4e1cb37",
+    "url": "/random-quote-machine/static/js/main.00c1dde7.chunk.js"
+  },
+  {
+    "revision": "6fefced6fd426e504746",
+    "url": "/random-quote-machine/static/js/runtime~main.296aa1cf.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/random-quote-machine/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/random-quote-machine/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/random-quote-machine/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/random-quote-machine/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/random-quote-machine/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
